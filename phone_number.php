cb2d57a47386e317fda5bc042ed91cd20e786e8a
Andrew <games.soroko@mail.ru>
2024-01-24 20:10:09 +0300

    received a clean number without symbols

--- phone_number.php
+++ phone_number.php
@@ -1,6 +1,19 @@
 <?php 
 
-$phones_list_JSON = file_get_contents("https://cdn.jsdelivr.net/gh/andr-04/inputmask-multi@master/data/phone-codes.json");
-$phones_list_array = json_decode($phones_list_JSON);
+function checkPhoneNumber(string $number)
+{
+    $phones_list_JSON = file_get_contents("https://cdn.jsdelivr.net/gh/andr-04/inputmask-multi@master/data/phone-codes.json");
+    $phones_list_array = json_decode($phones_list_JSON);
+    
+    // echo '<pre>' . print_r($phones_list_array, true) . '</pre>';
 
-echo '<pre>' . print_r($phones_list_array, true) . '</pre>';
+    $symbols_list = ['+', '-', '(', ')', ' '];
+
+    $clean_number = str_replace($symbols_list, '', $number);
+
+    echo $clean_number;
+}    
+
+
+checkPhoneNumber('+375(29)123-45-67');
+
