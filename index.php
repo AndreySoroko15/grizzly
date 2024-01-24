b8ff64cac23e7515a57ffbdfb443748a7769f605
Andrew <games.soroko@mail.ru>
2024-01-25 01:32:20 +0300

    added cookie-block to the page

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <!-- styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"   
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
     <script src="https://code.jquery.com/jquery-3.7.1.js"
             integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
             crossorigin="anonymous">
@@ -18,13 +20,24 @@
     <script src="./js/async_check_phone.js"></script>
 </head>
 <body>
-    <section class="check_number d-flex justify-content-center align-items-center flex-column">
-        <h1 class="fs-5 mt-3 mb-2">Задание №1. Проверка номера</h1>
-        <form action="./phone_number.php" method="post" class="d-flex" id="check_phone_form">
-            <input name="phone_number" type="tel" placeholder="Введите номер..." class="form-control">
-            <button type="submit" class="btn btn-primary ms-1">Отправить</button>
-        </form>
-        <p class="check_number_status"></p>
-    </section>
+    <div class="container">
+        <section class="check_number row">
+            <h1 class="fs-5 mt-3 mb-2 text-center">Задание №1. Проверка номера</h1>
+            <form action="./phone_number.php" method="post" class="col-lg-5 col-md-8 col-12 mx-auto" id="check_phone_form">
+                <div class="d-flex flex-column flex-sm-row">
+                    <input name="phone_number" type="tel" placeholder="Введите номер..." class="form-control">
+                    <button type="submit" class="btn btn-primary mt-2 mt-sm-0">Отправить</button>
+                </div>
+            </form>
+            <p class="check_number_status text-center"></p>
+        </section>
+    </div>
+
+    <div class="cookie_block position-fixed bottom-0 end-0 m-3">
+        <div class="alert alert-light d-flex justify-content-center flex-column" role="alert">
+            <p>Этот сайт использует файлы cookie для хранения данных. Продолжая использовать сайт, вы даете свое согласие на работу с этими файлами. </p>
+            <button type="button" class="btn btn-success" id="acceptCookie">Хорошо, принимаю</button>
+        </div>
+    </div>
 </body>
 </html>
