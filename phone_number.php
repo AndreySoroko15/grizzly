7defa4d3cb1b6667803931809b59db35a342ecd1
Andrew <games.soroko@mail.ru>
2024-01-24 22:04:00 +0300

    code is working, but with some mistakes

--- phone_number.php
+++ phone_number.php
@@ -1,28 +1,24 @@
 <?php 
 
-function checkPhoneNumber(string $number)
+function checkPhoneNumber(string $number, $cdn = 'https://cdn.jsdelivr.net/gh/andr-04/inputmask-multi@master/data/phone-codes.json')
 {
-    $phones_list_JSON = file_get_contents("https://cdn.jsdelivr.net/gh/andr-04/inputmask-multi@master/data/phone-codes.json");
+    $phones_list_JSON = file_get_contents($cdn);
     $phones_list_array = json_decode($phones_list_JSON);
     
-    // echo '<pre>' . print_r($phones_list_array, true) . '</pre>';
-
     $symbols_list = ['+', '-', '(', ')', ' ', '#'];
     $clean_number = str_replace($symbols_list, '', $number);
 
-    $country_code = str_replace($symbols_list, '', $phones_list_array[40]->mask);
-
-    echo $clean_number . '<br>' . $country_code . '<br>';
-
-    $cropped_number_under_mask = substr($clean_number, 0, strlen($country_code));
-
-    // echo '<p style="color: red">' . $cropped_number_under_mask . '</p>';
-
+    foreach($phones_list_array as $phone_list_array) {
+        $country_code = str_replace($symbols_list, '', $phone_list_array->mask);
+        $cropped_number_under_mask = substr($clean_number, 0, strlen($country_code));
+        
+        if($cropped_number_under_mask === $country_code) {
+            
+            return "Данный номер пренадлежит стране: $phone_list_array->name_ru";
+        }
+    }
     
-
-
+    return "Совпадений не найдено!";
 }    
 
-
-checkPhoneNumber('+375(29)123-45-67');
-
+echo checkPhoneNumber('+375(29)123-45-67');
